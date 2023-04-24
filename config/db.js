8e9c2f75701cae16e5751a34afe0d59310bed578
valentinasanchez12 <valen12345lara@gmail.com>
2023-04-23 22:23:51 -0500

    back

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const conectarDB = async () => {
   try {
-    const db = await mongoose.connect(process.env.MONGO_URI, {
+    const db = await mongoose.set('strictQuery', true); mongoose.connect(process.env.MONGO_URI, {
+      
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
